ea8787ec92cbe68c3cfb29598fe745fb9d82e536
SakshiKiran <65603329+SakshiKiran@users.noreply.github.com>
2022-09-13 20:13:11 -0400

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,48 @@
+# print ("Hello world")
+# name = "sakshi"
+# name2 = "mansi"
+# print (name)
+# 
+# num1 = 10
+# num2 = 5
+# answer = num1+num2
+# print (answer)
+# 
+# name3 = None
+# print (name3)
+# 
+# PI = 3.14
+# print (PI)
+# 
+# set_of_num = [1,5,8]
+# print(sum(set_of_num))
+# 
+# my_integer_variable = 1000
+# my_float_varable = 3.5
+# my_complex_variable = 1+2
+# 
+# print (type(my_integer_variable))
+# print(num1**num2)
+# print(num1%num2)
+# 
+# print (name+" "+name2)
+# print ("%s %s" % (name,name2))
+# print (name,name2)
+# print (name.append(" mansi"))
+# 
+# 
+#
+
+one = int(input())
+two = int(input())
+#if type(input()) is int and type(input()) is int:
+addition = (one+two)
+subtraction = (one-two)
+multiplication = (one*two)
+power1 = (one**two)
+power2 = (two**one)
+division = (one/two)
+print ("Sum",addition,'\n',"difference",subtraction,'\n',"product",multiplication,'\n',one,"**",two,power1,'\n',two,"**",one,power2,'\n',"division",division)
+
+
+
